9aa7926c7b4801158e88a15d20a728dd162553eb
KarimE146 <karim666the@gmail.com>
2024-07-16 18:06:09 -0400

    sign status

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, useColorScheme, TouchableOpacity } from 'react-native';
 import { Link } from 'expo-router';
+import { getAuth, onAuthStateChanged, User, signOut } from 'firebase/auth';
 
 interface Button16Props {
   children: React.ReactNode;
@@ -28,17 +29,88 @@ const LoginButton: React.FC<LoginButtonProps> = ({ children, href }) => (
   </Link>
 );
 
+
+
+interface LogoutButtonProps {
+  children: React.ReactNode;
+}
+
+const auth = getAuth();
+
+const LogoutButton: React.FC<LogoutButtonProps> = ({ children }) => {
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      console.log('User signed out successfully');
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
+  return (
+    <TouchableOpacity 
+      style={styles.loginButton} 
+      onPress={handleLogout}
+    >
+      <Text style={styles.loginButtonText}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+
+const checkUserLoginStatus = (callback: (user: User | null) => void): void => {
+  const auth = getAuth();
+  
+  onAuthStateChanged(auth, (user: User | null) => {
+    if (user) {
+      console.log(`User is signed in with UID: ${user.uid}`);
+    } else {
+      console.log("No user is signed in.");
+    }
+    callback(user);
+  });
+};
+
+
+
+
+
 const SettingsScreen = () => {
   const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === 'dark';
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    checkUserLoginStatus((currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={[styles.heading, {color: isDarkMode ? '#fff' : '#000'}]}>Settings</Text>
-        <LoginButton href="/login">
-          Login
-        </LoginButton>
+
+      
+        {user ? (
+          <LogoutButton>
+            Log out
+          </LogoutButton>
+        ) : (
+          <LoginButton href="/login">
+            Log in
+          </LoginButton>
+        )}
+
+
+
+
+
+
+        {/* <LoginButton href="/login">
+          Log in
+        </LoginButton> */}
+
       </View>
 
       <Button16 href="/settings/about">
